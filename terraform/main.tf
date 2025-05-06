d236a8521b8469a6c3ab314ef25eeede8ba3e6e3
Vishrut <vishrut010@gmail.com>
2025-05-07 02:04:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "static_site" {
-  bucket = "salesforce-clone-vishrut"  # replace with a unique name
-  acl    = "public-read"
+  bucket = "salesforce-clone-vishrut-2025"
 
   website {
     index_document = "index.html"
@@ -12,17 +11,34 @@ resource "aws_s3_bucket" "static_site" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.static_site.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.static_site.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "public_policy" {
   bucket = aws_s3_bucket.static_site.id
 
   policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow",
-        Principal = "*",
-        Action = ["s3:GetObject"],
-        Resource = ["${aws_s3_bucket.static_site.arn}/*"]
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
       }
     ]
   })
